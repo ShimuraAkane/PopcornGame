fd7aa3393273f52b48dfe29976f0f887f8d32048
TakebayashiiHaruka <s18b710@shohoku.ac.jp>
2019-10-24 16:28:14 +0900

    画面遷移の仕方を変えたよ

--- app/src/main/java/jp/ac/shohoku/teamu/popcorngame/MainActivity.java
+++ app/src/main/java/jp/ac/shohoku/teamu/popcorngame/MainActivity.java
@@ -14,23 +14,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.title_view);
-        Button button1 = (Button) findViewById(R.id.startButton);
-        button1.setOnClickListener(new OnClickListener(){
-            @Override
-            public void onClick(View v){
-                if(count == 1){
-                    LinearLayout layout = (LinearLayout) findViewById(R.id.titlelayout);
-                    layout.removeAllViews();
-                    getLayoutInflater().inflate(R.layout.play_view, layout);
-                    count = 2;
-                }else if(count == 2){
-                    LinearLayout layout = (LinearLayout) findViewById(R.id.playlayout);
-                    layout.removeAllViews();
-                    getLayoutInflater().inflate(R.layout.title_view, layout);
-                    count = 1;
-                }
-            }
-        });
+        setContentView(R.layout.activity_main);
     }
 }
